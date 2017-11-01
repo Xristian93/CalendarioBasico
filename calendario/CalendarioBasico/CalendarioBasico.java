c0c40a4beec7592a73a1e08e2023207987047d7a
Xristian93 <cristianfuente72@gmail.com>
2017-11-01 15:50:30 +0100

    Añadido metodo para imprimir la fecha

--- calendario/CalendarioBasico/CalendarioBasico.java
+++ calendario/CalendarioBasico/CalendarioBasico.java
@@ -36,4 +36,39 @@ public class CalendarioBasico
         }
     }
 
+    /**
+     * Print calendar date
+     */
+    public String printDate()
+    {
+        String date;
+        String dayString;
+        String monthString;
+        String yearString;
+        dayString = String.valueOf(day);
+        monthString = String.valueOf(month);
+        yearString = String.valueOf(year);
+
+        if (dayString.length() == 1){
+            dayString = "0" + dayString.substring(0,1);
+        }
+        else{
+            dayString = dayString.substring(0);
+        }
+        if (monthString.length() == 1){
+            monthString = "0" + monthString.substring(0,1);
+        }
+        else{
+            monthString = monthString.substring(0);
+        }
+        if (yearString.length() == 1){
+            yearString = "0" + yearString.substring(0,1);
+        }
+        else{
+            yearString = yearString.substring(0);
+        }
+
+        date = dayString + ("-") + monthString + ("-") + yearString;
+        return date;
+    }
 }
